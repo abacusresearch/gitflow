d638cd2779159b879a5dfbd1612c2521404d06ed
soggier <samuel.oggier@gmail.com>
2017-09-08 21:53:34 +0200

    setup basic integration testing

--- test/integration/flow.py
+++ test/integration/flow.py
@@ -2,31 +2,87 @@ import os
 import subprocess
 from tempfile import TemporaryDirectory
 
+import pytest
+
 from gitflow import __main__
 
 
+@pytest.mark.slow
 class TestFlow:
     tempdir: TemporaryDirectory
+    orig_cwd: str
+    git_origin: str
+    git_working_copy: str
+
+    def git_flow(self, *args) -> int:
+        return __main__.main([__name__, '-Bvv'] + [*args])
+
+    def git(self, *args) -> int:
+        proc = subprocess.Popen(args=['git'] + [*args])
+        proc.wait()
+        return proc.returncode
+
+    def git_get_commit_count(self) -> int:
+        proc = subprocess.Popen(args=['git', 'rev-list', '--all', '--count'],
+                                stdout=subprocess.PIPE)
+        out, err = proc.communicate()
+        assert proc.returncode == os.EX_OK
+        return int(out.decode('utf-8').splitlines()[0])
+
+    def commit(self, message: str = None):
+        if message is None:
+            message = "Test Commit #" + str(self.git_get_commit_count())
+        exit_code = self.git('commit', '--allow-empty', '-m', message)
+        assert exit_code == os.EX_OK
+
+    def add(self, *files: str):
+        exit_code = self.git('add', *files)
+        assert exit_code == os.EX_OK
+
+    def push(self):
+        exit_code = self.git('push', 'origin')
+        assert exit_code == os.EX_OK
 
     def setup_method(self, method):
+        self.orig_cwd = os.getcwd()
         self.tempdir = TemporaryDirectory()
 
-        os.chdir(self.tempdir.name)
+        self.git_origin = os.path.join(self.tempdir.name, 'origin.git')
+        self.git_working_copy = os.path.join(self.tempdir.name, 'working_copy.git')
 
-        proc = subprocess.Popen(args=['git', 'init'])
+        proc = subprocess.Popen(args=['git', 'init', '--bare', self.git_origin])
+        proc.wait()
+        assert proc.returncode == os.EX_OK
+        proc = subprocess.Popen(args=['git', 'clone', self.git_origin, self.git_working_copy])
         proc.wait()
         assert proc.returncode == os.EX_OK
 
-        with open(os.path.join(self.tempdir.name, 'gitflow.properties'), 'w') as property_file:
-            property_file.write('')
+        # switch to the working copy
+        os.chdir(self.git_working_copy)
 
-        pass
+        # create the config file
+        config_file = os.path.join(self.git_working_copy, 'gitflow.properties')
+        with open(config_file, 'w+') as property_file:
+            property_file.write('')
+            property_file.close()
+
+        # create & push the initial commit
+        self.add(config_file)
+        self.commit('initial commit: gitflow config file')
+        self.push()
 
     def teardown_method(self, method):
         self.tempdir.cleanup()
-        pass
+        os.chdir(self.orig_cwd)
 
     def test_status(self):
-        exit_code = __main__.main([__name__, 'status'])
+        exit_code = self.git_flow('status')
         assert exit_code == os.EX_OK
-        pass
+
+    def test_log(self):
+        exit_code = self.git_flow('log')
+        assert exit_code == os.EX_OK
+
+    def test_bump_major(self):
+        exit_code = self.git_flow('bump-major', '--assume-yes')
+        assert exit_code == os.EX_OK
