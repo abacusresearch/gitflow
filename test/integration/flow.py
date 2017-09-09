5179b13832489c5c3608e4fbd5bc7e0ce58df29d
soggier <samuel.oggier@gmail.com>
2017-09-09 15:37:41 +0200

    test work branches

--- test/integration/flow.py
+++ test/integration/flow.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 import subprocess
 from tempfile import TemporaryDirectory
@@ -46,13 +47,21 @@ class TestFlow:
         exit_code = self.git('add', *files)
         assert exit_code == os.EX_OK
 
-    def push(self):
-        exit_code = self.git('push', 'origin')
+    def push(self, *args):
+        exit_code = self.git('push', *args)
         assert exit_code == os.EX_OK
 
     def checkout(self, branch: str):
         exit_code = self.git('checkout', branch)
         assert exit_code == os.EX_OK
+
+    def current_head(self):
+        proc = subprocess.Popen(args=['git', 'rev-parse', '--revs-only', '--symbolic-full-name', 'HEAD'],
+                                stdout=subprocess.PIPE)
+        out, err = proc.communicate()
+        assert proc.returncode == os.EX_OK
+        current_head = out.decode('utf-8').splitlines()[0]
+        return current_head
 
     def assert_same_elements(self, expected: list, actual: list):
         expected = sorted(expected)
@@ -64,6 +73,10 @@ class TestFlow:
         if actual is None:
             actual = self.list_refs()
         self.assert_same_elements(expected, actual)
+
+    def assert_head(self, expected: str):
+        current_head = self.current_head()
+        assert current_head == expected
 
     def setup_method(self, method):
         self.orig_cwd = os.getcwd()
@@ -297,3 +310,63 @@ class TestFlow:
             'refs/tags/sequential_version/2',
             'refs/tags/version/1.0.0-alpha.2'
         ])
+
+    def test_begin_end_dev_feature(self):
+        self.assert_head('refs/heads/master')
+
+        exit_code = self.git_flow('begin', 'dev', 'feature', 'test-feature')
+        assert exit_code == os.EX_OK
+
+        self.assert_head('refs/heads/dev/feature/test-feature')
+
+        for _ in itertools.repeat(None, 3):
+            self.commit()
+        self.push('-u', 'origin', 'dev/feature/test-feature')
+        exit_code = self.git_flow('end', 'dev', 'feature', 'test-feature')
+        assert exit_code == os.EX_OK
+
+        self.assert_head('refs/heads/master')
+
+
+    def test_begin_end_prod_fix(self):
+        self.assert_head('refs/heads/master')
+
+        exit_code = self.git_flow('bump-major', '--assume-yes')
+        assert exit_code == os.EX_OK
+        self.assert_refs([
+            'refs/heads/master',
+            'refs/remotes/origin/master',
+            # 'refs/heads/release/1.0',  # local branch
+            'refs/remotes/origin/release/1.0',
+            'refs/tags/sequential_version/1',
+            'refs/tags/version/1.0.0-alpha.1'
+        ])
+
+        self.assert_head('refs/heads/master')
+
+        self.checkout('release/1.0')
+        self.assert_head('refs/heads/release/1.0')
+
+        exit_code = self.git_flow('begin', 'prod', 'fix', 'test-fix')
+        assert exit_code == os.EX_OK
+        self.assert_refs([
+            'refs/heads/master',
+            'refs/remotes/origin/master',
+
+            'refs/heads/release/1.0',  # local branch
+            'refs/remotes/origin/release/1.0',
+            'refs/tags/sequential_version/1',
+            'refs/tags/version/1.0.0-alpha.1',
+
+            'refs/heads/prod/fix/test-fix'
+        ])
+
+        self.assert_head('refs/heads/prod/fix/test-fix')
+
+        for _ in itertools.repeat(None, 3):
+            self.commit()
+        self.push('-u')
+        exit_code = self.git_flow('end', 'prod', 'fix', 'test-fix', '1.0')
+        assert exit_code == os.EX_OK
+
+        self.assert_head('refs/heads/release/1.0')
