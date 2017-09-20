cac9fd462979358cf8f57c32716295bf7f6ca3ce
soggier <samuel.oggier@gmail.com>
2017-09-20 10:38:41 +0200

    error handling

--- gitflow/procedures/build.py
+++ gitflow/procedures/build.py
@@ -1,6 +1,7 @@
+import os
 import subprocess
 
-from gitflow import utils
+from gitflow import utils, _
 from gitflow.context import Context
 from gitflow.procedures import get_command_context
 
@@ -14,9 +15,20 @@ def call(context: Context):
     for stage in context.config.build_stages:
         for step in stage.steps:
             for command in step.commands:
-                proc = subprocess.Popen(args=command,
-                                 stdin=subprocess.PIPE,
-                                 cwd=context.root)
-                proc.wait()
+                try:
+                    proc = subprocess.Popen(args=command,
+                                            stdin=subprocess.PIPE,
+                                            cwd=context.root)
+                    proc.wait()
+                    if proc.returncode != os.EX_OK:
+                        command_context.fail(os.EX_DATAERR,
+                                             _("Build failed."),
+                                             _("Stage {stage}:{step} returned with an error.")
+                                             .format(stage=stage.name, step=step.name))
+                except FileNotFoundError as e:
+                    command_context.fail(os.EX_DATAERR,
+                                         _("Build failed."),
+                                         _("Stage {stage}:{step} could not be executed.")
+                                         .format(stage=stage.name, step=step.name))
 
     return command_context.result
