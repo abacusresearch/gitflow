e2f3f62da756a114754c4e000525f5b4e14f43c4
Samuel Oggier <samuel.oggier@abacus.ch>
2018-07-12 10:53:13 +0200

    fixed the version algorithms

--- gitflow/procedures/scheme/scheme_procedures.py
+++ gitflow/procedures/scheme/scheme_procedures.py
@@ -9,57 +9,101 @@ from gitflow.const import VersioningScheme
 from gitflow.version import VersionConfig
 
 
+def filter_sequence_number(version_config, prev_version, global_seq):
+    if version_config.versioning_scheme == VersioningScheme.SEMVER_WITH_SEQ:
+        if prev_version is not None:
+            version_seq = semver.parse_version_info(prev_version).prerelease
+            if version_seq is not None:
+                # must throw if not an integer
+                version_seq = int(version_seq)
+
+                if global_seq is None:
+                    raise ValueError('the version sequence number is defined, while the global one is not')
+                if version_seq > global_seq:
+                    raise ValueError('the version sequence number is greater than the global sequence number')
+        if global_seq is None:
+            global_seq = 0
+    else:
+        global_seq = None
+
+    return global_seq
+
+
 def version_bump_major(version_config: VersionConfig, version: Optional[str], global_seq: Optional[int]):
     result = Result()
-    version_info = semver.parse_version_info(semver.bump_major(version))
-    pre_release = True
 
-    result.value = semver.format_version(
-        version_info.major,
-        version_info.minor,
-        version_info.patch,
-        (version_config.qualifiers[0] + ".1" if pre_release else None)
-        if version_config.versioning_scheme != VersioningScheme.SEMVER_WITH_SEQ
-        else (global_seq or 0) + 1,
-        None)
+    try:
+        global_seq = filter_sequence_number(version_config, version, global_seq)
+    except ValueError as e:
+        result.error(os.EX_DATAERR, "version increment failed", str(e))
+
+    if not result.has_errors():
+        version_info = semver.parse_version_info(
+            semver.bump_major(version)) if version is not None else semver.parse_version_info("0.0.0")
+        pre_release = True
+
+        result.value = semver.format_version(
+            version_info.major,
+            version_info.minor,
+            version_info.patch,
+            (version_config.qualifiers[0] + ".1" if pre_release else None)
+            if version_config.versioning_scheme != VersioningScheme.SEMVER_WITH_SEQ
+            else global_seq + 1,
+            None)
     return result
 
 
 def version_bump_minor(version_config: VersionConfig, version: Optional[str], global_seq: Optional[int]):
     result = Result()
-    version_info = semver.parse_version_info(semver.bump_minor(version))
-    pre_release = True
 
-    result.value = semver.format_version(
-        version_info.major,
-        version_info.minor,
-        version_info.patch,
-        (version_config.qualifiers[0] + ".1" if pre_release else None)
-        if version_config.versioning_scheme != VersioningScheme.SEMVER_WITH_SEQ
-        else (global_seq or 0) + 1,
-        None)
+    try:
+        global_seq = filter_sequence_number(version_config, version, global_seq)
+    except ValueError as e:
+        result.error(os.EX_DATAERR, "version increment failed", str(e))
+
+    if not result.has_errors():
+        version_info = semver.parse_version_info(
+            semver.bump_minor(version)) if version is not None else semver.parse_version_info("0.0.0")
+        pre_release = True
+
+        result.value = semver.format_version(
+            version_info.major,
+            version_info.minor,
+            version_info.patch,
+            (version_config.qualifiers[0] + ".1" if pre_release else None)
+            if version_config.versioning_scheme != VersioningScheme.SEMVER_WITH_SEQ
+            else global_seq + 1,
+            None)
     return result
 
 
 def version_bump_patch(version_config: VersionConfig, version: Optional[str], global_seq: Optional[int]):
     result = Result()
-    version_info = semver.parse_version_info(semver.bump_patch(version))
-    pre_release = True
 
-    result.value = semver.format_version(
-        version_info.major,
-        version_info.minor,
-        version_info.patch,
-        (version_config.qualifiers[0] + ".1" if pre_release else None)
-        if version_config.versioning_scheme != VersioningScheme.SEMVER_WITH_SEQ
-        else (global_seq or 0) + 1,
-        None)
+    try:
+        global_seq = filter_sequence_number(version_config, version, global_seq)
+    except ValueError as e:
+        result.error(os.EX_DATAERR, "version increment failed", str(e))
+
+    if not result.has_errors():
+        version_info = semver.parse_version_info(
+            semver.bump_patch(version)) if version is not None else semver.parse_version_info("0.0.0")
+        pre_release = True
+
+        result.value = semver.format_version(
+            version_info.major,
+            version_info.minor,
+            version_info.patch,
+            (version_config.qualifiers[0] + ".1" if pre_release else None)
+            if version_config.versioning_scheme != VersioningScheme.SEMVER_WITH_SEQ
+            else global_seq + 1,
+            None)
     return result
 
 
 def version_bump_qualifier(version_config: VersionConfig, version: Optional[str], global_seq: Optional[int]):
     result = Result()
-    version_info = semver.parse_version_info(version)
+    version_info = semver.parse_version_info(version) if version is not None else semver.parse_version_info("0.0.0")
 
     new_qualifier = None
 
@@ -113,7 +157,7 @@ def version_bump_qualifier(version_config: VersionConfig, version: Optional[str]
 
 def version_bump_prerelease(version_config: VersionConfig, version: Optional[str], global_seq: Optional[int]):
     result = Result()
-    version_info = semver.parse_version_info(version)
+    version_info = semver.parse_version_info(version) if version is not None else semver.parse_version_info("0.0.0")
 
     if version_info.prerelease:
         prerelease_version_elements = version_info.prerelease.split(".")
@@ -160,7 +204,7 @@ def version_bump_prerelease(version_config: VersionConfig, version: Optional[str
 
 def version_bump_to_release(version_config: VersionConfig, version: Optional[str], global_seq: Optional[int]):
     result = Result()
-    version_info = semver.parse_version_info(version)
+    version_info = semver.parse_version_info(version) if version is not None else semver.parse_version_info("0.0.0")
 
     if version_config.versioning_scheme == VersioningScheme.SEMVER_WITH_SEQ:
         result.error(os.EX_USAGE,
--- test/unit/semver_with_seq.py
+++ test/unit/semver_with_seq.py
@@ -3,30 +3,33 @@ from gitflow.procedures.scheme import scheme_procedures
 from gitflow.version import VersionConfig
 
 config = VersionConfig()
-config.versioning_scheme = VersioningScheme.SEMVER
-config.qualifiers = ['alpha', 'beta']
+config.versioning_scheme = VersioningScheme.SEMVER_WITH_SEQ
 
 
 def test_major_increment():
-    assert scheme_procedures.version_bump_major(config, "0.0.0", None).value == "1.0.0-alpha.1"
-    assert scheme_procedures.version_bump_major(config, "1.0.0-beta.4", None).value == "2.0.0-alpha.1"
+    assert scheme_procedures.version_bump_major(config, None, None).value == "0.0.0-1"
+    assert scheme_procedures.version_bump_major(config, "0.0.0-1", None).value is None
+    assert scheme_procedures.version_bump_major(config, "0.0.0-1", 1).value == "1.0.0-2"
+    assert scheme_procedures.version_bump_major(config, "0.0.0-1", 5).value == "1.0.0-6"
 
 
 def test_minor_increment():
-    assert scheme_procedures.version_bump_minor(config, "0.0.0", None).value == "0.1.0-alpha.1"
-    assert scheme_procedures.version_bump_minor(config, "1.0.0-beta.4", None).value == "1.1.0-alpha.1"
+    assert scheme_procedures.version_bump_minor(config, None, None).value == "0.0.0-1"
+    assert scheme_procedures.version_bump_minor(config, "0.0.0-1", None).value is None
+    assert scheme_procedures.version_bump_minor(config, "0.0.0-1", 1).value == "0.1.0-2"
 
 
 def test_patch_increment():
-    assert scheme_procedures.version_bump_patch(config, "0.0.0", None).value == "0.0.1-alpha.1"
-    assert scheme_procedures.version_bump_patch(config, "1.0.5", None).value == "1.0.6-alpha.1"
-    assert scheme_procedures.version_bump_patch(config, "1.0.0-beta.4", None).value == "1.0.1-alpha.1"
+    assert scheme_procedures.version_bump_patch(config, None, None).value == "0.0.0-1"
+    assert scheme_procedures.version_bump_patch(config, "1.0.5-2", None).value is None
+    assert scheme_procedures.version_bump_patch(config, "1.0.5-2", 1).value is None
+    assert scheme_procedures.version_bump_patch(config, "1.0.5-2", 2).value == "1.0.6-3"
 
 
 def test_qualifier_increment():
     assert scheme_procedures.version_bump_qualifier(config, "1.0.0", None).value is None
-    assert scheme_procedures.version_bump_qualifier(config, "1.0.0-alpha.4", None).value == "1.0.0-beta.1"
-    assert scheme_procedures.version_bump_qualifier(config, "1.0.0-beta.4", None).value is None
+    assert scheme_procedures.version_bump_qualifier(config, "1.0.0-4", None).value is None
+    assert scheme_procedures.version_bump_qualifier(config, "1.0.0-4", 4).value is None
 
 
 def test_pre_release_increment():
@@ -37,5 +40,6 @@ def test_pre_release_increment():
 
 
 def test_increment_to_release():
-    assert scheme_procedures.version_bump_to_release(config, "1.0.0-alpha.4", None).value == "1.0.0"
+    assert scheme_procedures.version_bump_to_release(config, "1.0.0-4", None).value is None
+    assert scheme_procedures.version_bump_to_release(config, "1.0.0-4", 4).value is None
     assert scheme_procedures.version_bump_to_release(config, "1.0.0", None).value is None
